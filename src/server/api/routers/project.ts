257e73d7a5a2cb3b334488b416472cbc31b76fa1
Krish-Panchani <krishpanchani1346@gmail.com>
2024-12-26 19:55:11 +0530

    Integrate GitHub commit polling into project creation and update userToProject relation naming

--- src/server/api/routers/project.ts
+++ src/server/api/routers/project.ts
@@ -1,5 +1,6 @@
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
+import { pollCommits } from "@/lib/github";
 
 export const projectRouter = createTRPCRouter({
     createProject: protectedProcedure.input(
@@ -13,20 +14,21 @@ export const projectRouter = createTRPCRouter({
             data: {
                 githubUrl: input.githubUrl,
                 name: input.projectName,
-                UserToProject: {
+                userToProject: {
                     create: {
                         userId: ctx.user.userId!,
                     }
                 }
            }
         })
+        await pollCommits(project.id)
         return project;
     }),
 
     getProjects: protectedProcedure.query(async ({ ctx }) => {
         return await ctx.db.project.findMany({
             where: {
-                UserToProject: {
+                userToProject: {
                     some: {
                         userId: ctx.user.userId!
                     }
